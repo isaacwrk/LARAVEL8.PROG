b389e9c7cc5dd1597bcf513df34f34e9743b4a51
Isaac <isaacsouzawrk@gmail.com>
2020-12-07 18:35:51 -0300

    controllers e routes prontas

---
+++ README.md
@@ -0,0 +1,5 @@
+## SOBRE O PROJETO ## <br>
+estou trabalhando em um novo CRUD com laravel 8<br>
+atualmente já estabeleci routes e controllers, trabalhando agora na parte de views e blade.<br>
+
+##### LET'S CODE ####
---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    public function index(){
+        return view('produtos.index');
+
+    }
+
+    public function create(){
+        return view('produtos.create');
+        
+    }
+
+    public function show($nome, $valor){
+        return view('produtos.show', ['nome'=>$nome, 'valor'=>$valor]);
+    }
+}
